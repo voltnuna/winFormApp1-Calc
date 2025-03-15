98e699dc281814cb346def937c15b534d5a8708e
kimdoeun <doviisdev.git@gmail.com>
2025-03-15 13:22:48 +0900

    first commit

---
+++ Calculator.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Calculator
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.HelloLabel = new System.Windows.Forms.Label();
+            this.sum1 = new System.Windows.Forms.TextBox();
+            this.sum2 = new System.Windows.Forms.TextBox();
+            this.sumBtn = new System.Windows.Forms.Button();
+            this.result = new System.Windows.Forms.TextBox();
+            this.result2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // HelloLabel
+            // 
+            this.HelloLabel.AutoSize = true;
+            this.HelloLabel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.HelloLabel.Location = new System.Drawing.Point(23, 26);
+            this.HelloLabel.Name = "HelloLabel";
+            this.HelloLabel.Size = new System.Drawing.Size(109, 12);
+            this.HelloLabel.TabIndex = 0;
+            this.HelloLabel.Text = "여기를 클릭하세요.";
+            this.HelloLabel.Click += new System.EventHandler(this.HelloLabel_Click);
+            // 
+            // sum1
+            // 
+            this.sum1.Location = new System.Drawing.Point(32, 90);
+            this.sum1.Name = "sum1";
+            this.sum1.Size = new System.Drawing.Size(100, 21);
+            this.sum1.TabIndex = 1;
+            this.sum1.Text = "1";
+            this.sum1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // sum2
+            // 
+            this.sum2.Location = new System.Drawing.Point(138, 90);
+            this.sum2.Name = "sum2";
+            this.sum2.Size = new System.Drawing.Size(100, 21);
+            this.sum2.TabIndex = 2;
+            // 
+            // sumBtn
+            // 
+            this.sumBtn.Location = new System.Drawing.Point(244, 90);
+            this.sumBtn.Name = "sumBtn";
+            this.sumBtn.Size = new System.Drawing.Size(75, 23);
+            this.sumBtn.TabIndex = 3;
+            this.sumBtn.Text = "=";
+            this.sumBtn.UseVisualStyleBackColor = true;
+            this.sumBtn.Click += new System.EventHandler(this.sumBtn_Click);
+            // 
+            // result
+            // 
+            this.result.Location = new System.Drawing.Point(32, 135);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(100, 21);
+            this.result.TabIndex = 4;
+            // 
+            // result2
+            // 
+            this.result2.Location = new System.Drawing.Point(32, 162);
+            this.result2.Name = "result2";
+            this.result2.Size = new System.Drawing.Size(100, 21);
+            this.result2.TabIndex = 5;
+            this.result2.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(418, 265);
+            this.Controls.Add(this.result2);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.sumBtn);
+            this.Controls.Add(this.sum2);
+            this.Controls.Add(this.sum1);
+            this.Controls.Add(this.HelloLabel);
+            this.Name = "Calculator";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label HelloLabel;
+        private System.Windows.Forms.TextBox sum1;
+        private System.Windows.Forms.TextBox sum2;
+        private System.Windows.Forms.Button sumBtn;
+        private System.Windows.Forms.TextBox result;
+        private System.Windows.Forms.TextBox result2;
+    }
+}
+
---
+++ Calculator.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Calculator : Form
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void HelloLabel_Click(object sender, EventArgs e)
+        {
+            int num1 = 1;
+            int num2 = 2;
+            int sum = num1 + num2;
+            HelloLabel.Text = sum.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        //name이 sumBtn인 Button을 Click시 실행되는 메서드 
+        private void sumBtn_Click(object sender, EventArgs e)
+        {
+            int number1 = Convert.ToInt32(sum1.Text); // Int로 형변환 기본이 32bit
+            int number2 = Convert.ToInt32(sum2.Text);
+
+            result.Text = addCalc(number1, number2);
+            result2.Text = subCalc(number1, number2);
+        }
+
+
+        public string addCalc(int a, int b)
+        {
+            return (a + b).ToString(); //모든 데이터타입이 ToString메서드 가지고 있다.
+        }
+   
+        public string subCalc(int a, int b)
+        {
+            return (a - b).ToString(); //모든 데이터타입이 ToString메서드 가지고 있다.
+        }
+    }
+}
+/*
+ * 메소드는 클래스에 속한 함수를 말함 메소드는 함수인데 클래스에 속한 함수를 메소드라고 부름
+함수는 함수 이름과 매개변수로 프로토타입이 결정됨 매개변수가 다르다면 동일한 이름의 함수를 여러개 만들 수 있다.
+이걸 오버로딩이라고 한다. 함수의 리턴형은 함수의 프로토타입이 결정되는 데에 어떠한 역할도 하지 않음.  
+
+ 
+ */
+
