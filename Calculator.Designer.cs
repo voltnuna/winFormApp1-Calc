21d9fdb68bce05381030c13073fdf2d46a97625d
kimdoeun <doviisdev.git@gmail.com>
2025-03-15 14:53:46 +0900

    data validation practice

--- Calculator.Designer.cs
+++ Calculator.Designer.cs
@@ -29,92 +29,20 @@ namespace WindowsFormsApp1
         /// </summary>
         private void InitializeComponent()
         {
-            this.HelloLabel = new System.Windows.Forms.Label();
-            this.sum1 = new System.Windows.Forms.TextBox();
-            this.sum2 = new System.Windows.Forms.TextBox();
-            this.sumBtn = new System.Windows.Forms.Button();
-            this.result = new System.Windows.Forms.TextBox();
-            this.result2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
-            // 
-            // HelloLabel
-            // 
-            this.HelloLabel.AutoSize = true;
-            this.HelloLabel.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.HelloLabel.Location = new System.Drawing.Point(23, 26);
-            this.HelloLabel.Name = "HelloLabel";
-            this.HelloLabel.Size = new System.Drawing.Size(109, 12);
-            this.HelloLabel.TabIndex = 0;
-            this.HelloLabel.Text = "여기를 클릭하세요.";
-            this.HelloLabel.Click += new System.EventHandler(this.HelloLabel_Click);
-            // 
-            // sum1
-            // 
-            this.sum1.Location = new System.Drawing.Point(32, 90);
-            this.sum1.Name = "sum1";
-            this.sum1.Size = new System.Drawing.Size(100, 21);
-            this.sum1.TabIndex = 1;
-            this.sum1.Text = "1";
-            this.sum1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            // 
-            // sum2
-            // 
-            this.sum2.Location = new System.Drawing.Point(138, 90);
-            this.sum2.Name = "sum2";
-            this.sum2.Size = new System.Drawing.Size(100, 21);
-            this.sum2.TabIndex = 2;
-            // 
-            // sumBtn
-            // 
-            this.sumBtn.Location = new System.Drawing.Point(244, 90);
-            this.sumBtn.Name = "sumBtn";
-            this.sumBtn.Size = new System.Drawing.Size(75, 23);
-            this.sumBtn.TabIndex = 3;
-            this.sumBtn.Text = "=";
-            this.sumBtn.UseVisualStyleBackColor = true;
-            this.sumBtn.Click += new System.EventHandler(this.sumBtn_Click);
-            // 
-            // result
-            // 
-            this.result.Location = new System.Drawing.Point(32, 135);
-            this.result.Name = "result";
-            this.result.Size = new System.Drawing.Size(100, 21);
-            this.result.TabIndex = 4;
-            // 
-            // result2
-            // 
-            this.result2.Location = new System.Drawing.Point(32, 162);
-            this.result2.Name = "result2";
-            this.result2.Size = new System.Drawing.Size(100, 21);
-            this.result2.TabIndex = 5;
-            this.result2.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
             // 
             // Calculator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(418, 265);
-            this.Controls.Add(this.result2);
-            this.Controls.Add(this.result);
-            this.Controls.Add(this.sumBtn);
-            this.Controls.Add(this.sum2);
-            this.Controls.Add(this.sum1);
-            this.Controls.Add(this.HelloLabel);
             this.Name = "Calculator";
             this.Text = "Form1";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label HelloLabel;
-        private System.Windows.Forms.TextBox sum1;
-        private System.Windows.Forms.TextBox sum2;
-        private System.Windows.Forms.Button sumBtn;
-        private System.Windows.Forms.TextBox result;
-        private System.Windows.Forms.TextBox result2;
     }
 }
 
--- Calculator.cs
+++ Calculator.cs
@@ -17,6 +17,9 @@ namespace WindowsFormsApp1
             InitializeComponent();
         }
 
+        private void textBox1_TextChanged(object sender, EventArgs e) { }
+        private void textBox1_TextChanged_1(object sender, EventArgs e) { }
+
         private void HelloLabel_Click(object sender, EventArgs e)
         {
             int num1 = 1;
@@ -25,26 +28,43 @@ namespace WindowsFormsApp1
             HelloLabel.Text = sum.ToString();
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox1_TextChanged_1(object sender, EventArgs e)
-        {
-
-        }
-
         //name이 sumBtn인 Button을 Click시 실행되는 메서드 
         private void sumBtn_Click(object sender, EventArgs e)
         {
-            int number1 = Convert.ToInt32(sum1.Text); // Int로 형변환 기본이 32bit
-            int number2 = Convert.ToInt32(sum2.Text);
+
+            int number1 = 0;
+            int number2 = 0;
+
+            //String.IsNullOrWhiteSpace
+            //문자열이 null, 비어 있음("") 또는 공백 문자로만 구성되어 있는지를 확인하는 데 사용됩니다.
+
+            if (String.IsNullOrWhiteSpace(sum1.Text) || String.IsNullOrWhiteSpace(sum2.Text)) {
+                MessageBox.Show("숫자를 입력해주세요!");
+                sum1.SelectAll(); //text전체 블럭으로 선택됨
+                sum1.Focus(); //textbox에 포커스1
+                //clearText();
+                
+                return ;
+            }
+            // int.TryParse는 int형 값을 String으로 변환해보고 성공하면 number1이라는 변수에 저장 결과에 따라 true false 반환
+            if (int.TryParse(sum1.Text, out number1) == false) {
+                MessageBox.Show("Sum1에 문자가 들어왔습니다!");
+                clearText();
+                return;
+            } else if (int.TryParse(sum2.Text, out number2) == false) { 
+                MessageBox.Show("Sum2에 숫자를 입력하세요!");
+                clearText();
+                return;
+            }
 
             result.Text = addCalc(number1, number2);
             result2.Text = subCalc(number1, number2);
         }
 
+        public void clearText(){
+            sum1.Text = "";
+            sum2.Text = "";
+        }
 
         public string addCalc(int a, int b)
         {
@@ -61,7 +81,10 @@ namespace WindowsFormsApp1
  * 메소드는 클래스에 속한 함수를 말함 메소드는 함수인데 클래스에 속한 함수를 메소드라고 부름
 함수는 함수 이름과 매개변수로 프로토타입이 결정됨 매개변수가 다르다면 동일한 이름의 함수를 여러개 만들 수 있다.
 이걸 오버로딩이라고 한다. 함수의 리턴형은 함수의 프로토타입이 결정되는 데에 어떠한 역할도 하지 않음.  
-
- 
  */
+
+
+/*          int number1 = Convert.ToInt32(sum1.Text); // Int로 형변환 기본이 32bit
+            int number2 = Convert.ToInt32(sum2.Text);
+*/
 
